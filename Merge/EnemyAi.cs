6e26a441286914aab7e1214a7c5896b219f7abf7
Kelly Ung <kelung@chapman.edu>
2026-04-23 15:24:30 -0700

    Changed detection radius and patrol speed

--- Merge/EnemyAi.cs
+++ Merge/EnemyAi.cs
@@ -1,7 +1,7 @@
 ﻿public class EnemyAi
 {
-    public float DetectionRadius = 0f;
-    public float PatrolSpeed = 0f;
+    public float DetectionRadius = 7000f;
+    public float PatrolSpeed = 800f;
 
     public void Patrol()
     {
