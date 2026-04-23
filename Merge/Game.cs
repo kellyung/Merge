95926819c963cac4c7d12800336e6cef2e2cc8b7
Kelly Ung <kelung@chapman.edu>
2026-04-23 15:01:17 -0700

    Changed starting lives

--- Merge/Game.cs
+++ Merge/Game.cs
@@ -1,8 +1,7 @@
 ﻿public class Game
 {
-    public int StartingLives = 0;
+    public int StartingLives = 3; // ← changed from 0
     public int Score = 0;
-
     public void AddScore(int points)
     {
         Score += points;
