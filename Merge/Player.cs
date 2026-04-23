b96306678221ad72043b1c55f90da5b4d33c89e5
Kelly Ung <kelung@chapman.edu>
2026-04-23 15:08:24 -0700

    Secret

--- Merge/Player.cs
+++ Merge/Player.cs
@@ -1,8 +1,8 @@
 ﻿public class Player
 {
     // TODO: branches will replace these 0 placeholders
-    public float MoveSpeed = 0f;
-    public float JumpForce = 0f;
+    public float MoveSpeed = 1000f;
+    public float JumpForce = 30000f;
 
     public void Move(float horizontal)
     {
